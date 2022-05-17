72838e5dcb385458676044e291ac315e9823d879
sunxinyi0501 <sunxinyi@udel.edu>
2022-05-17 12:41:15 -0400

    first try

--- src/helloworkd.java
+++ src/helloworkd.java
@@ -1,5 +1,8 @@
-package PACKAGE_NAME;/**
-* Creat By xinyi on 2022/5/17.
-*
-*/public class helloworkd {
+/**
+ * Creat By xinyi on 2022/5/17.
+ */
+public class helloworkd {
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
 }
